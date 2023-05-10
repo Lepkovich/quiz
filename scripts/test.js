7b7e5da1718678e285d3c1e2de63d3928824c0f1
Lepkovich <6332133@gmail.com>
2023-05-10 18:58:52 +0300

    working on test.js from work. not done

--- scripts/test.js
+++ scripts/test.js
@@ -3,9 +3,11 @@
         quiz: null,
         questionTitleElement: null,
         nextButtonElement: null,
+        passButtonElement: null,
         prevButtonElement: null,
         optionsElement: null,
         currentQuestionIndex: 1,
+        userResult: [],
         init() {
             checkUserData();
             const url = new URL(location.href);
@@ -32,8 +34,16 @@
             console.log(this.quiz);
             this.questionTitleElement = document.getElementById('title');
             this.optionsElement = document.getElementById('options');
+
             this.nextButtonElement = document.getElementById('next');
+            this.nextButtonElement.onclick = this.move.bind(this, 'next');
+
+            this.passButtonElement = document.getElementById('pass');
+            this.passButtonElement.onclick = this.move.bind(this, 'pass');
+
             this.prevButtonElement = document.getElementById('prev');
+            this.prevButtonElement.onclick = this.move.bind(this, 'prev');
+
             this.showQuestion();
         },
         showQuestion() {
@@ -42,6 +52,7 @@
                 + ':</span> ' + activeQuestion.question;
             this.optionsElement.innerHTML = ''; //удалим текущие ответы
             const that = this;
+            const chosenOption = this.userResult.find(item => item.questionId === activeQuestion.id); //чтобы отрисовать сделанный ранее пользователем выбор radio, ищем есть в массиве этот выбор
             //и размещаем структуру html с вариантами ответов
             activeQuestion.answers.forEach(answer => {
 //создаем строку <div class="test-question-option">
@@ -50,10 +61,15 @@
 //создаем строку <input type="radio" id="answer-one" name="answer">
                 const inputId = 'answer-' + answer.id
                 const inputElement = document.createElement('input');
+                inputElement.className = 'option-answer';
                 inputElement.setAttribute('id', inputId);
                 inputElement.setAttribute('type', 'radio');
                 inputElement.setAttribute('name', 'answer');
                 inputElement.setAttribute('value', answer.id);
+                if (chosenOption && chosenOption.chosenAnswerId === answer.id) {
+                    inputElement.setAttribute('checked', 'checked');
+                }
+
 
                 inputElement.onchange = function () {
                     that.chooseAnswer();
@@ -76,14 +92,59 @@
                 optionElement.appendChild(labelElement);
                 this.optionsElement.appendChild(optionElement);
 
-            })
+            });
+            if (chosenOption && chosenOption.chosenAnswerId) {
+                this.nextButtonElement.removeAttribute('disabled');
+            } else {
+                this.nextButtonElement.setAttribute('disabled', 'disabled');
+            }
+            if (this.currentQuestionIndex === this.quiz.questions.length) {
+                this.nextButtonElement.innerText = 'Завершить';
+            } else {
+                this.nextButtonElement.innerText = 'Далее';
+            }
+            if (this.currentQuestionIndex > 1) {
+                this.prevButtonElement.removeAttribute('disabled');
+            } else {
+                this.prevButtonElement.setAttribute('disabled', 'disabled');
+            }
         },
         chooseAnswer() {
             this.nextButtonElement.removeAttribute('disabled');
         },
-        move() {
-            //видео 1:21
+        move(action) {
+            const activeQuestion = this.quiz.questions[this.currentQuestionIndex -1];
+            const chosenAnswer = Array.from(document.getElementsByClassName('option-answer')).find(element => {
+                return element.checked;
+            })
+            let chosenAnswerId = null;
+            if (chosenAnswer && chosenAnswer.value) {
+                chosenAnswerId = Number(chosenAnswer.value);
+            }
+
+            const existingResult = this.userResult.find(item => {
+                return item.questionId === activeQuestion.id
+            });
+            if (existingResult) {
+                existingResult.chosenAnswerId = chosenAnswerId;
+            } else {
+                this.userResult.push({
+                    questionId: activeQuestion.id,
+                    chosenAnswerId: chosenAnswerId
+                });
+            }
+
+            console.log(this.userResult);
+
+            if (action === 'next' || action === 'pass') {
+                this.currentQuestionIndex++;
+            } else {
+                this.currentQuestionIndex--;
+            }
+            this.showQuestion();
         }
     }
     Test.init();
 })();
+
+// урок на 1:43
