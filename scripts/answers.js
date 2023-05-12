402a816377854436a2fcfc27485ff227d962c2fd
Lepkovich <6332133@gmail.com>
2023-05-12 21:52:17 +0300

    answers.js from home

--- scripts/answers.js
+++ scripts/answers.js
@@ -6,7 +6,7 @@
             // const testId = url.searchParams.get('id');
 //сначала выведем наши вопросы
                 const xhr = new XMLHttpRequest();
-                xhr.open('GET', 'https://testologia.site/get-quiz?id=3', false);
+                xhr.open('GET', 'https://testologia.site/get-quiz?id=2', false);
                 xhr.send();
                 if (xhr.status === 200 && xhr.responseText) {
                     try {
@@ -34,36 +34,36 @@
             console.log('Наши вопросы:')
 
             for (let a = 0; a < this.quiz.questions.length; a++) {
-                // questionTitle.innerHTML = '<span>Вопрос ' + (a+1) + ':</span> ' + this.quiz.questions[a].question;
                 console.log(this.quiz.questions[a].question);
-                const questionTitle = document.createElement('div');
-                questionTitle.className = 'test-answers-block-title';
+                // this.quiz.questions[a].answers.forEach(item => {
+                //     console.log(item.answer);
+                // });
             }
+
+            console.log('Варианты ответов:')
 
 //меняем название теста по id (1,2,3) в поле name
 
-            document.getElementById('pre-title').innerText = 'Измененный заголовок';
+            document.getElementById('pre-title').innerText = this.quiz.name;
             const answersBlock = document.getElementById('answers-block')
 //создаем структуру html
 //             <div className="test-answers-block-title" id="title">
 //                 <span>Вопрос 1:</span> Тут должен быть текст вопроса
 //             </div>
 
-            for (let i=1; i<5;i++) {
+            for (let i=0; i < this.quiz.questions.length; i++) {
 // Создаем div-элемент с классом "test-answers-block-title" и идентификатором "title"
                 const div = document.createElement("div");
                 div.classList.add("test-answers-block-title");
-                div.id = "title";
-                div.innerHTML = '<span>Вопрос ' + i + ':</span> ' + 'Измененный вопрос';
+                div.id = "question";
+                div.innerHTML = '<span>Вопрос ' + (i+1) + ':</span> ' + this.quiz.questions[i].question;
 
 // Добавляем созданный div-элемент в документ
                 answersBlock.appendChild(div);
 
-
-
-                // questionTitle.innerHTML = '<span>Вопрос ' + (i+1) + ':</span> ' + 'Измененный вопрос';
-                for (let k = 1; k < 5; k++) {
-// создаем div-элемент
+                this.quiz.questions[i].answers.forEach(item => {
+                    console.log(item.answer);
+                    // создаем div-элемент
                     const div = document.createElement("div");
                     div.classList.add("test-answers-block-options");
                     div.id = "options";
@@ -81,15 +81,14 @@
 // создаем текст для первого варианта ответа
                     const optionText = document.createElement("div");
                     optionText.classList.add("test-answers-block-option-text");
-                    optionText.innerText = "Вариант ответа " + k;
+                    optionText.innerText = item.answer;
                     option1.appendChild(optionText);
 
 // добавляем первый вариант ответа в div-элемент
                     div.appendChild(option1);
                     // добавляем div-элемент в документ
                     answersBlock.appendChild(div);
-
-                }
+                });
             }
         }
     }
