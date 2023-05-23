2e4222e614050572aa8a4a3874bee104e2c7306a
Lepkovich <6332133@gmail.com>
2023-05-23 17:26:56 +0300

    25m of lesson

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
+import {Router} from "./router.js";
+
 class App {
     constructor() {
-        
+        this.router = new Router();
+        window.addEventListener('DOMContentLoaded', () => {
+            this.router.openRoute();
+        })
     }
 }
+
+(new App()); // при загрузке index.html мы перейдем в App.js и сразу чтобы класс App сработал, создаем здесь новый экземпляр
+// в скобки обернули для красоты
--- src/router.js
+++ src/router.js
@@ -1,13 +1,39 @@
+import {Form} from "./components/form.js";
+
 export class Router {
     constructor() {
         this.routes = [
             {
                 route: '#/',
                 title: 'Главная',
-                temlate: 'templates/index.html',
+                template: 'templates/index.html',
                 styles: 'styles/index.css',
-            }
+                load: () => {
+                }
+            },
+            {
+                route: 'form',
+                title: 'Регистрация',
+                template: 'templates/form.html',
+                styles: 'styles/form.css',
+                load: () => {
+                    new Form();
+                }
+            },
         ]
 
+    }
+
+    openRoute() { // функция выберет какой роут загрузить исходя из текста в адресной строке
+        const newRoute = this.routes.find(item => {
+            return item.route === window.location.hash;
+        })
+
+        if(!newRoute) { // если мы не найдем в адресной строке ничего из перечисленного в routes, загрузим главную страницу нашего SPA
+            window.location.href = '#/';
+            return; // обязательно нужно завершить эту функцию, чтобы дальше ничего за ней не происходило.
+        }
+
+            document.getElementById('content').innerHTML = await fetch(newRoute.template)
     }
 }
