357bf3545731b23bcb6bd2d247b1b692ad14ebfd
Lepkovich <6332133@gmail.com>
2023-06-14 22:42:08 +0300

    lesson is done. no mistakes

--- src/components/choice.js
+++ src/components/choice.js
@@ -44,7 +44,7 @@ export class Choice {
                 choiceOptionArrowElement.className = 'choice-option-arrow';
 
                 const choiceOptionImageElement = document.createElement('img');
-                choiceOptionImageElement.setAttribute('src', 'static/images/arrow.png');
+                choiceOptionImageElement.setAttribute('src', '/images/arrow.png');
                 choiceOptionImageElement.setAttribute('alt', 'стрелка');
 
                 choiceOptionArrowElement.appendChild(choiceOptionImageElement);
