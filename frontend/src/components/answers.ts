40edc80a52b1b0a06e196482a904ff851be9bb6a
Lepkovich <6332133@gmail.com>
2023-09-06 18:47:23 +0300

    working on answers.ts 7 errors Object is possibly null

--- frontend/src/components/answers.ts
+++ frontend/src/components/answers.ts
@@ -2,13 +2,13 @@ import {UrlManager} from "../utils/url-manager";
 import {Auth} from "../services/auth";
 import {CustomHttp} from "../services/custom-http";
 import config from "../../config/config";
-import {QuizType} from "../types/quiz.type";
+import {QuizResultType, QuizType} from "../types/quiz.type";
 import {QueryParamsType} from "../types/query-params.type";
-import {UserInfoType} from "../types/user-info.type";
+import {DefaultResponseType} from "../types/default-response.type";
 
 export class Answers {
 
-    private quiz: QuizType | null;
+    private quiz: QuizType | QuizResultType | null;
     private routeParams: QueryParamsType;
     private userData: string | null;
     constructor() {
@@ -18,26 +18,26 @@ export class Answers {
         this.init();
 
     }
-    async init() {
+    private async init(): Promise<void> {
         const userInfo  = Auth.getUserInfo(); //берем из localStorage информацию о пользователе
         const userEmail = Auth.getUserEmail(); //берем из localStorage email
         if(!userInfo || !userEmail){
             location.href = '#/';
         } else {
             if (userInfo.fullName) {
-                this.userData = userInfo.fullName  + ', ' + userEmail;
+                this.userData as string = userInfo.fullName  + ', ' + userEmail;
             }
 
             if(this.routeParams.id) {
                 try {
                     if (userInfo.userId) {
-                        const result = await CustomHttp.request(config.host + '/tests/' + this.routeParams.id + '/result/details?userId=' + userInfo.userId);
+                        const result: DefaultResponseType | QuizResultType  = await CustomHttp.request(config.host + '/tests/' + this.routeParams.id + '/result/details?userId=' + userInfo.userId);
                         // /api/tests/:id/result/details?userId=:userId
                         if(result) {
-                            if (result.error) {
-                                throw new Error(result.error);
+                            if ((result as DefaultResponseType).error) {
+                                throw new Error((result as DefaultResponseType).message as string);
                             }
-                            this.quiz = result;
+                            this.quiz = result as QuizResultType;
                             this.showQuestions();
                         }
                     }
@@ -47,64 +47,75 @@ export class Answers {
             }
         }
     }
-    showQuestions() {
+    private showQuestions(): void {
 
-        document.getElementById('pre-title').innerText = this.quiz.test.name;
-        document.getElementById('user').querySelector('span').textContent = this.userData;
+        if (!this.quiz) return;
 
-        const answersBlock = document.getElementById('answers-block')
+        if ('test' in this.quiz) {
+            const quizResult = this.quiz as QuizResultType;
+            if (quizResult.test.name) {
+                document.getElementById('pre-title').innerText = quizResult.test.name as string;
+            }
+            if (this.userData) {
+                document.getElementById('user').querySelector('span').textContent = this.userData as string;
+            }
+
+            const answersBlock: HTMLElement | null = document.getElementById('answers-block')
 
 //создаем структуру html
-        for (let i = 0; i < this.quiz.test.questions.length; i++) {
+            for (let i = 0; i < quizResult.test.questions.length; i++) {
 
 // Создаем div-элемент с class="test-answers-block-title" и id="title"
-            const div = document.createElement("div");
-            div.classList.add("test-answers-block-title");
-            div.id = "question";
-            div.innerHTML = '<span>Вопрос ' + (i + 1) + ':</span> ' + this.quiz.test.questions[i].question;
+                const div = document.createElement("div");
+                div.classList.add("test-answers-block-title");
+                div.id = "question";
+                div.innerHTML = '<span>Вопрос ' + (i + 1) + ':</span> ' + quizResult.test.questions[i].question;
 
 // Добавляем созданный div-элемент в answersBlock
-            answersBlock.appendChild(div);
+                answersBlock.appendChild(div);
 
-            this.quiz.test.questions[i].answers.forEach(item => {
+                quizResult.test.questions[i].answers.forEach(item => {
 // создаем div-элемент с class="test-answers-block-options" и id="options"
-                const div = document.createElement("div");
-                div.classList.add("test-answers-block-options");
-                div.id = "options";
+                    const div = document.createElement("div");
+                    div.classList.add("test-answers-block-options");
+                    div.id = "options";
 
 // создаем вариант ответа
-                const option1 = document.createElement("div");
-                option1.classList.add("test-answers-block-option");
+                    const option1 = document.createElement("div");
+                    option1.classList.add("test-answers-block-option");
 
 // создаем кружок перед ответом
 
-                const circle = document.createElement("div");
-                circle.classList.add("test-answers-block-option-circle");
-                if (item.correct === true) {
-                    circle.classList.add("correct-answer-circle");
-                } else if (item.correct === false) {
-                    circle.classList.add("wrong-answer-circle");
-                }
-                option1.appendChild(circle);
+                    const circle = document.createElement("div");
+                    circle.classList.add("test-answers-block-option-circle");
+                    if (item.correct === true) {
+                        circle.classList.add("correct-answer-circle");
+                    } else if (item.correct === false) {
+                        circle.classList.add("wrong-answer-circle");
+                    }
+                    option1.appendChild(circle);
 
 // создаем текст для варианта ответа
-                const optionText = document.createElement("div");
-                optionText.classList.add("test-answers-block-option-text");
-                if (item.correct === true) {
-                    optionText.classList.add("correct-answer-text");
-                } else if (item.correct === false) {
-                    optionText.classList.add("wrong-answer-text");
-                }
-                optionText.innerText = item.answer;
-                option1.appendChild(optionText);
+                    const optionText = document.createElement("div");
+                    optionText.classList.add("test-answers-block-option-text");
+                    if (item.correct === true) {
+                        optionText.classList.add("correct-answer-text");
+                    } else if (item.correct === false) {
+                        optionText.classList.add("wrong-answer-text");
+                    }
+                    optionText.innerText = item.answer;
+                    option1.appendChild(optionText);
 
 // добавляем вариант ответа в div-элемент
-                div.appendChild(option1);
+                    div.appendChild(option1);
 // добавляем div-элемент в answersBlock
-                answersBlock.appendChild(div);
-            });
+                    answersBlock.appendChild(div);
+                });
+
+            }
 
         }
+
         const backToResult = document.getElementById('back-to-results');
         backToResult.onclick = function () {
             window.history.back();
